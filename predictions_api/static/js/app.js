c4a157e7fa0beff48eb8388af23df4e47899d3a1
Ayushi Desai <ayushi2494@gmail.com>
2022-07-17 22:21:32 -0400

    handeled special characters

--- predictions_api/static/js/app.js
+++ predictions_api/static/js/app.js
@@ -5,25 +5,32 @@ jQuery(document).ready(function () {
         events: {
         search: function (qry, callback) {
                // let's do a custom ajax call
-               $.ajax({
-                   url: 'http://localhost:5000/predict?value='+qry+'&max_sequence_len=3&nested_list_len=4' ,
-                   type: "post",
-                   contentType: "application/json",
-                   dataType: "json",
-                   beforeSend: function () {
-                       $('.overlay').show()
-                   },
-                   complete: function () {
-                       $('.overlay').hide()
-                   }
-               }).done(function (jsondata, textStatus, jqXHR) {
-                callback($.map(jsondata['data'], function (item) {
-                      return item.toString()
-                }));
-      
-              }).fail(function (jsondata, textStatus, jqXHR) {
-                   console.log(jsondata)
-               });
+               let regex = /[^a-zA-Z]+/
+                  if(qry.match(regex))
+                  {
+                    callback([]);
+                  }
+                  else
+                  {
+                    $.ajax({
+                        url: 'http://localhost:5000/predict?value='+qry+'&max_sequence_len=3&nested_list_len=4' ,
+                        type: "post",
+                        contentType: "application/json",
+                        dataType: "json",
+                        beforeSend: function () {
+                            $('.overlay').show()
+                        },
+                        complete: function () {
+                            $('.overlay').hide()
+                        }
+                    }).done(function (jsondata, textStatus, jqXHR) {
+                         callback($.map(jsondata['data'], function (item) {
+                               return item.toString()
+                         }));
+                    }).fail(function (jsondata, textStatus, jqXHR) {
+                          console.log(jsondata)
+                      });
+                  }
             },
         },
        formatResult: function (item) {
